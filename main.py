d393dfffaf92bdf0b5a5c45e05e451419522e18f
Egor <iwdkmp@gmail.com>
2023-01-27 06:06:45 +0300

    second iteration, update code.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 print ('хай, это супер калькулятор☺')
-first_number = float(input('Введите первое число: '))
-second_number = float(input('Введите второе число: '))
-mathematical_action = input('Введите действие которое хотите выполнить (доступны + - * /) : ')
-if mathematical_action == '+':
-    print ('Ответ: ' "%.2f" % (first_number+second_number))
-elif mathematical_action == '-':
-    print ('Ответ: ' "%.2f" % (first_number-second_number))
-elif mathematical_action == '*':
-    print ('Ответ: ' "%.2f" % (first_number*second_number))
-else:
-    print ('Ответ: '"%.2f" % (first_number/second_number))
+count = 0
+while count != 1:
+    first_number = int(input('Введите первое число: '))
+    second_number = int(input('Введите второе число: '))
+    mathematical_action = input('Введите действие которое хотите выполнить (доступны + - * /) : ')
+    if mathematical_action == '+':
+        print('Ответ: ' "%.2f" % (first_number+second_number))
+        count += 1
+    elif mathematical_action == '-':
+        print('Ответ: ' "%.2f" % (first_number-second_number))
+        count += 1
+    elif mathematical_action == '*':
+        print('Ответ: ' "%.2f" % (first_number*second_number))
+        count += 1
+    elif mathematical_action == '/':
+        print('Ответ: '"%.2f" % (first_number/second_number))
+    else:
+        print('Попробуйте еще раз. Возможно вы неправильно ввели операцию или число. ')
